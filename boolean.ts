34f0812cd92b635448f21609dd8638a08d21cc6c
Ricardo Escalante <ej_ricardo@yahoo.com>
2022-08-24 00:29:07 -0500

    cambio mensajes ts

--- boolean.ts
+++ boolean.ts
@@ -4,5 +4,5 @@ let soy_fan: boolean = false;
 soy_alto = soy_fan || soy_alto; 
 console.log(soy_alto, "soy_fan");
 console.log(soy_fan, "soy_fan");
-console.log(soy_alto && soy_fan);
-console.log(soy_alto || soy_fan);
+console.log(soy_alto && soy_fan, "conjuncion");
+console.log(soy_alto || soy_fan, "disyuncion") ;
