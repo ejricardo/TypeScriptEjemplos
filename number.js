8245852cf26bdfee1ff481258c57e86b4f5ab552
Ricardo Escalante <ej_ricardo@yahoo.com>
2022-08-24 01:37:20 -0500

    se agregan nuevos archivos compilados JS

---
+++ number.js
@@ -0,0 +1,10 @@
+var mi_edad = 23;
+var mi_nota = 16.55;
+var mi_nota2 = 18.55;
+//operadores aritmeticos 
+console.log(mi_nota + mi_nota2);
+console.log(mi_nota - mi_nota2);
+console.log(mi_nota * mi_nota2);
+console.log(mi_nota / mi_nota2);
+console.log(mi_nota % mi_nota2);
+console.log(6 % 3);
---
+++ string.js
@@ -0,0 +1,11 @@
+var pelicula = "Mission Imposible";
+var serie = 'Hanibal';
+var poema = "dfd\ndsgdf\nsdfg\nsdfgsdf\nlorem\ndfd";
+var programas = " Mis programas favoritos son : " + pelicula + " y " + serie;
+console.log(programas);
+//template string
+var programas2 = "\nMis programas favoritos son ".concat(pelicula, " y ").concat(serie, "\n");
+console.log(programas2);
+var edad_actual = 32;
+var edad_futura = "El siguiente mes mi edad sera ".concat(edad_actual + 1);
+console.log(edad_futura);
