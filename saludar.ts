b8d1759558f6941a6f55fcbd95c1cad8d7a5040e
Ricardo Escalante <ej_ricardo@yahoo.com>
2022-08-24 00:27:07 -0500

    estos son los typescripts

---
+++ saludar.ts
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+	return " hola " + nombre
+}
+
+let mi_nombre = "Francisco"
+let resultado = saludar(mi_nombre)
+
---
+++ variable.ts
@@ -0,0 +1,9 @@
+//declarar
+let nombre_de_mi_variable;
+//inicializar
+nombre_de_mi_variable=true;
+//declara e inicializar
+
+let equipo = "Real Madrid esto es una prueba 4";
+//constantes
+const pi = 3.14
